714c61f3f6323ae5c4effcd2c728a23355bead54
siunix <abd.siunix@gmail.com>
2022-04-27 14:49:59 +0700

    Redux implemented for get github user list

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Search.sass'
+import { useDispatch } from 'react-redux'
+import { gitAccountList } from '../../actions/git'
 
 const Search = () => {
+  const dispatch = useDispatch()
+  const [keyword, setKeyword] = useState("")
+
+  const getList = () => {
+    dispatch(gitAccountList(keyword))
+  }
+
+  useEffect(() => {
+    getList()
+  }, [])
+
   return (
     <div className='container mx-auto'>
       <div className='search'>
         <input
           className='search__input'
-          placeholder='Cari username disini'
+          placeholder='Find here'
+          value={keyword}
+          onChange={e => setKeyword(e.target.value)}
         />
         <button
           className='search__button'
+          onClick={() => getList()}
         >
           Search
         </button>
